ce2e61b8b8996790265011f4356871c74e889c9d
Mark Huot <mark@markhuot.com>
2012-11-09 08:30:22 -0500

    Removing extraneous asset calls.

--- controllers/base.php
+++ controllers/base.php
@@ -1,18 +1,8 @@
 <?php
 
-use Assetic\Asset\AssetCollection;
-use Assetic\Asset\FileAsset;
-use Assetic\Asset\GlobAsset;
-
 class Keystone_Base_Controller extends Controller {
 
   public $restful = true;
-
-  public function before()
-  {
-    Keystone\Assetic::add('css', Bundle::path('keystone').'public/css/screen.css');
-    Keystone\Assetic::add('css', Bundle::path('keystone').'public/fontawesome/css/font-awesome.css');
-  }
 
   /**
    * Catch-all method for requests that can't be matched.
