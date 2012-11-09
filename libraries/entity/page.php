14f4833d8fc2723514e9d9334dac8960feb0d378
Mark Huot <mark@markhuot.com>
2012-11-09 16:55:09 -0500

    removing comments

--- libraries/entity/page.php
+++ libraries/entity/page.php
@@ -17,53 +17,10 @@ class Page extends \Keystone\Entity
 
   public function set_regions($regions=array())
   {
-    // if (is_array($regions) && !empty($regions)) {
-    //   foreach ($regions as $region => &$fields) {
-    //     foreach ($fields as $index => &$field) {
-    //       $type = $field['type'];
-    //       if (file_exists($path = path('fields').$type.'/field.php')) {
-    //         require_once $path;
-    //         $class = ucfirst($type).'_Field';
-    //         if (class_exists($class)) {
-    //           $obj = new $class;
-    //           if (method_exists($obj, 'save')) {
-    //             $field = $obj->save($field);
-    //           }
-    //         }
-    //       }
-    //     }
-    //   }
-    // }
-
     $this->attributes['regions'] = $regions;
     $this->attributes['title'] = 'title';
     $this->attributes['excerpt'] = 'excerpt';
   }
-
-  // public function get_regions()
-  // {
-  //   $regions = @$this->attributes['regions'];
-
-  //   if (is_array($regions)) {
-  //     foreach ($regions as $region => &$fields) {
-  //       foreach ($fields as $index => &$field) {
-  //         $type = $field['type'];
-  //         if (file_exists($path = path('fields').$type.'/field.php')) {
-  //           require_once $path;
-  //           $class = ucfirst($type).'_Field';
-  //           if (class_exists($class)) {
-  //             $obj = new $class;
-  //             if (method_exists($obj, 'get')) {
-  //               $field = $obj->get($field);
-  //             }
-  //           }
-  //         }
-  //       }
-  //     }
-  //   }
-
-  //   return $regions;
-  // }
 
   public function layout_form()
   {
