25e34f5cc101aa0a78a47f31c8836c129c8ff296
Mark Huot <mark@markhuot.com>
2012-11-10 07:59:10 -0500

    adding private vars to "document" the entity

--- libraries/entity/page.php
+++ libraries/entity/page.php
@@ -4,15 +4,26 @@ namespace Keystone\Entity;
 
 class Page extends \Keystone\Entity
 {
+
+  protected $_id;
+  protected $_language;
+  protected \Keystone\Layout $_layout;
+  protected $_regions;
+  protected \Keystone\Uri $_uri;
+  protected $_title;
+  protected $_excerpt;
+  protected $_published;
+  protected \Keystone\DateTime $_published_at;
+
   protected $accessible = array(
-    'regions',
-    'layout',
-    'uri',
-    'published_at',
     'language',
+    'layout',
+    'regions',
+    'uri',
     'title',
     'excerpt',
-    'published'
+    'published',
+    'published_at'
   );
 
   public function set_regions($regions=array())
