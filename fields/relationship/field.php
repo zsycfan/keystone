069897a48586f664cc6788d9ad2f8fb62fbc44f6
Mark Huot <mark@markhuot.com>
2012-11-18 15:28:56 -0500

    fixing issue with undefined data

--- fields/relationship/field.php
+++ fields/relationship/field.php
@@ -3,7 +3,7 @@
 class Relationship_Field
 {
   public function get($data) {
-    if ($data['related']) {
+    if (@$data['related']) {
       if ($related = Keystone\Repository\Page::find($data['related'])) {
         $data['related'] = $related->to_array();
       }
