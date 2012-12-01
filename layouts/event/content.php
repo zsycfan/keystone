ac3b512e0a931dd6eb88e8b8ec4ddf3c3c33f936
Mark Huot <mark@markhuot.com>
2012-12-01 15:11:58 +0000

    layouts have the screen automatically added

--- layouts/event/content.php
+++ layouts/event/content.php
@@ -1,22 +1,22 @@
 <div class="row-fluid">
   <h1 class="span12">
-    <?= region(array('name' => 'content.title', 'allow' => 'plain', 'max' => 1, 'min' => 1)) ?>
+    <?= region(array('name' => 'title', 'allow' => 'plain', 'max' => 1, 'min' => 1)) ?>
   </h1>
 </div>
 
 <div class="row-fluid">
   <div class="span3">
-    <?= region(array('name' => 'content.sidebar', 'allow' => 'relationship')) ?>
+    <?= region(array('name' => 'sidebar', 'allow' => 'relationship')) ?>
   </div>
   <div class="span9">
     <div class="row-fluid">
       <div class="span12">
-        <?= region(array('name' => 'content.body', 'as' => 'excerpt')) ?>
+        <?= region(array('name' => 'body', 'as' => 'excerpt')) ?>
       </div>
     </div>
     <div class="row-fluid">
       <div class="span12">
-        <?= region(array('name' => 'content.postscript')) ?>
+        <?= region(array('name' => 'postscript')) ?>
       </div>
     </div>
   </div>
--- layouts/subpage/content.php
+++ layouts/subpage/content.php
@@ -1,28 +1,28 @@
 <div class="row-fluid">
   <h1 class="span12">
-    <?= region(array('name' => 'content.title', 'allow' => 'plain', 'max' => 1, 'min' => 1)) ?>
+    <?= region(array('name' => 'title', 'allow' => 'plain', 'max' => 1, 'min' => 1)) ?>
   </h1>
 </div>
 
 <div class="row-fluid">
   <div class="span3">
-    <?= region(array('name' => 'content.sidebar', 'allow' => 'relationship')) ?>
+    <?= region(array('name' => 'sidebar', 'allow' => 'relationship')) ?>
   </div>
   <div class="span9">
     <div class="row-fluid">
       <div class="span12">
-        <?= region(array('name' => 'content.body')) ?>
+        <?= region(array('name' => 'body')) ?>
       </div>
     </div>
     <div class="row-fluid">
       <div class="span4">
-        <?= region(array('name' => 'content.footer-left', 'allow' => array('asset', 'plain'))) ?>
+        <?= region(array('name' => 'footer-left', 'allow' => array('asset', 'plain'))) ?>
       </div>
       <div class="span4">
-        <?= region(array('name' => 'content.footer-center')) ?>
+        <?= region(array('name' => 'footer-center')) ?>
       </div>
       <div class="span4">
-        <?= region(array('name' => 'content.footer-right')) ?>
+        <?= region(array('name' => 'footer-right')) ?>
       </div>
     </div>
   </div>
