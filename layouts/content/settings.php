9279d81bf01e431685d9a1f60368b106ee6e1259
Mark Huot <mark@markhuot.com>
2012-12-04 03:04:42 -0500

    adding labels

--- layouts/content/settings.php
+++ layouts/content/settings.php
@@ -1,11 +1,13 @@
 <div class="row-fluid">
   <div class="span12">
-    <?= region(array('name' => 'uri', 'allow' => 'plain', 'max' => 1, 'min' => 1)) ?>
+    <p><strong>URI</strong></p>
+    <?= region(array('name' => 'uri', 'allow' => 'plain', 'max' => 1, 'min' => 1, 'config:plain:placeholder' => 'uri...')) ?>
   </div>
 </div>
 
 <div class="row-fluid">
   <div class="span12">
+    <p><strong>Tags</strong></p>
     <?= region(array('name' => 'tags', 'allow' => 'tags', 'max' => 1, 'min' => 1)) ?>
   </div>
 </div>
