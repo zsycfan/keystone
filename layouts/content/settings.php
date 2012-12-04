6a0fdaf688ddb569ee84fc48ced8f280c13c24b3
Mark Huot <mark@markhuot.com>
2012-12-04 03:01:12 -0500

    adding tags field

--- layouts/content/settings.php
+++ layouts/content/settings.php
@@ -3,3 +3,9 @@
     <?= region(array('name' => 'uri', 'allow' => 'plain', 'max' => 1, 'min' => 1)) ?>
   </div>
 </div>
+
+<div class="row-fluid">
+  <div class="span12">
+    <?= region(array('name' => 'tags', 'allow' => 'tags', 'max' => 1, 'min' => 1)) ?>
+  </div>
+</div>
