9c979dedd4c7be48933ea3c9ff797ad06a13c2f0
Mark Huot <mark@markhuot.com>
2012-12-09 13:10:58 +0000

    updated with new letter-spaced version of the token field

--- fields/tags/field.js
+++ fields/tags/field.js
@@ -1,114 +1,12 @@
-/** CLICK EVENTS **/
-
-$(document).on('click', function(event) {
-	$('.tags-field-selected').trigger('blur');
+$(document).on('click', '.ta-list a', function(e) {
+  var input = $(this).closest('.ta-typeahead').data('typeahead-input');
+  input.before('<input name="'+input.attr('data-typeahead-name')+'" value="'+$(this).data('data').id+'" />');
+  return false;
 });
-
-$(document).on('click', '.tags-field', function(event) {
-	$(this).find('span[contenteditable]').focus();
-});
-
-$(document).on('click', '.tags-field-input', function(event) {
-	event.stopPropagation();
-});
-
-$(document).on('click', '.tags-field-tag', function(event) {
-	if (!event.shiftKey) {
-		$('.tags-field-selected').trigger('blur');
-	}
-	$(this).trigger('focus', event.shiftKey);
-	event.stopPropagation();
-});
-
-/** KEYPRESS EVENTS **/
-
-$(document).on('keydown', function(event) {
-	var src = $(event.srcElement);
-	if (event.keyCode == 8 && src.text() == '') {
-		src.prevAll('.tags-field-tag').eq(0).trigger('focus');
-	}
-	else if (event.keyCode == 37) {
-		if (event.shiftKey) {
-			src.prevAll('.tags-field-tag').eq(0).trigger('togglefocus', event.shiftKey);
-		}
-		else {
-			src.prevAll('.tags-field-input').eq(0).trigger('focus', event.shiftKey);
-		}
-	}
-	else if (event.keyCode == 39) {
-		if (event.shiftKey) {
-			src.nextAll('.tags-field-tag:not(.tags-field-selected)').eq(0).trigger('togglefocus', event.shiftKey);
-		}
-		else {
-			src.nextAll('.tags-field-input').eq(0).trigger('focus', event.shiftKey);
-		}
-	}
-});
-
-$(document).on('keydown keyup', '.tags-field-phantom', function(event) {
-	var phantom = $(event.srcElement);
-	if (phantom.text() != '' || event.keyCode == 8) {
-		$('.tags-field-selected').remove();
-		phantom.prevAll('.tags-field-input:not(.tags-field-phantom)').eq(0).remove();
-		phantom.nextAll('.tags-field-input').eq(0).remove();
-		phantom.removeClass('tags-field-phantom');
-		phantom.css('position', 'static');
-		event.stopPropagation();
-	}
-});
-
-/** FOCUS EVENTS **/
-
-$(document).on('focus', '.tags-field', function(event) {
-	$(this).addClass('focused');
-});
-
-$(document).on('focus', '.tags-field-input:not(.tags-field-phantom)', function(event, preserveSelection) {
-	if (!preserveSelection) {
-		$('.tags-field-phantom').remove();
-		$('.tags-field-selected').trigger('blur');
-	}
-});
-
-$(document).on('focus', '.tags-field-tag', function(event, preserveSelection) {
-	$('.tags-field-phantom').remove();
-	$(this).prevAll('.tags-field-input').eq(0).clone(true).insertBefore(this).css({
-		'position': 'absolute',
-		'left': $(this).position().left - 9999,
-		'top': $(this).position().top + 0,
-	}).trigger('focus', preserveSelection).addClass('tags-field-phantom');
-	$(this).addClass('tags-field-selected');
-});
-
-$(document).on('togglefocus', '.tags-field-tag', function(event, preserveSelection) {
-	if ($(this).hasClass('tags-field-selected')) {
-		$(this).trigger('blur');
-	}
-	else {
-		$(this).trigger('focus', preserveSelection);
-	}
-});
-
-/** BLUR EVENTS **/
-
-$(document).on('blur', '.tags-field', function(event) {
-	var field = $(this);
-	setTimeout(function() {
-		if (!$('.tags-field-selected').size() && !field.find(':focus').size()) {
-			field.removeClass('focused');
-		}
-	}, 10);
-});
-
-$(document).on('blur', '.tags-field-tag', function(event) {
-	$(this).removeClass('tags-field-selected');
-});
-
-/** --------------------------------------------------------- **/
 
 $(function() {
 	$('[data-typeahead]').data('typeahead-renderRow', function(data) {
-		return '<li>'+data.tag+'</li>';
+		return '<li><a href="#" data-data="'+JSON.stringify(data).replace(/"/g,'&quot;')+'">'+data.tag+'</a></li>';
 	});
 });
 
@@ -117,6 +15,7 @@ $(document).on('keyup', '[data-typeahead]', function(event) {
 	var input = $(this);
 	var popover = input.data('typeahead-popover') || $('<div class="ta-typeahead"><ul class="ta-list"></ul><i class="icon-undo"></i></div>').hide().appendTo(document.body);
 	input.data('typeahead-popover', popover);
+	popover.data('typeahead-input', input);
 
 	// Localize api methods
 	var renderRow = input.data('typeahead-renderRow');
