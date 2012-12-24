8c0ae38708f556befbeb75bfe2dbfcd4fb58cbca
Mark Huot <mark@markhuot.com>
2012-12-24 08:00:40 -0500

    updated style

--- fields/tags/api.php
+++ fields/tags/api.php
@@ -3,13 +3,20 @@
 class Tags_Api
 {
   public function get_index() {
-    return Response::make(json_encode(array(
-      array("id" => 0, "text" => Input::get('q')),
+    $result = array(
       array("id" => 1, "text" => "one"),
       array("id" => 2, "text" => "two"),
       array("id" => 3, "text" => "three"),
       array("id" => 4, "text" => "four")
-    )), 200, array(
+    );
+
+    if ($term = Input::get('q')) {
+      array_unshift($result, array(
+        "id" => $term, "text" => $term
+      ));
+    }
+
+    return Response::make(json_encode($result), 200, array(
       'Content-type' => 'application/json'
     ));
   }
---
+++ fields/tags/field.php
@@ -0,0 +1,9 @@
+<?php
+
+class Tags_Field
+{
+  public function save($data)
+  {
+    print_r($data); die;
+  }
+}
