452a4b3e024e92d50b9345b16644f8800f890e0e
Mark Huot <mark@markhuot.com>
2013-01-05 06:08:30 -0500

    Update libraries/region.php

--- libraries/region.php
+++ libraries/region.php
@@ -14,10 +14,10 @@ class Region extends Object
 
   public static function make()
   {
-    throw new \Exception('Regions must be created with an explicit name. Try `Region::makeNamed(\'body\')` instead.');
+    throw new \Exception('Regions must be created with an explicit name. Try `Region::makeWithName(\'body\')` instead.');
   }
 
-  public static function makeNamed($name)
+  public static function makeWithName($name)
   {
     $obj = new static();
     $obj->name = $name;
