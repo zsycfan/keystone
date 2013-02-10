ebd801b9c2d5ff99b78341234fcc4e3dcea2b7ec
Mark Huot <mark@markhuot.com>
2013-02-10 09:03:40 -0500

    documenting

--- libraries/asset.md
+++ libraries/asset.md
@@ -5,19 +5,19 @@ The `Asset` class accepts any number of static files which will later be
 rendered to the display. It consists of a single magic method which accepts
 any type of asset to be called back later.
 
-To add a CSS file, for example you could run,
+To add a CSS file, for example, you could run:
 
 ```php
 Keystone\Asset::addCssFile('/path/to/file.css');
 ```
 
-Also available is passing in the raw source,
+Also available is passing in the raw source:
 
 ```php
 Keystone\Asset::addCss('* html * { display:none; }');
 ```
 
-Finally, you can retrieve any set asset with a similar get method,
+Finally, you can retrieve any asset with a similarly named get method:
 
 ```php
 Keystone\Asset::getCss();
--- libraries/asset.php
+++ libraries/asset.php
@@ -10,19 +10,19 @@ namespace Keystone;
  * rendered to the display. It consists of a single magic method which accepts
  * any type of asset to be called back later.
  *
- * To add a CSS file, for example you could run,
+ * To add a CSS file, for example, you could run:
  *
  * ```php
  * Keystone\Asset::addCssFile('/path/to/file.css');
  * ```
  *
- * Also available is passing in the raw source,
+ * Also available is passing in the raw source:
  *
  * ```php
  * Keystone\Asset::addCss('* html * { display:none; }');
  * ```
  *
- * Finally, you can retrieve any set asset with a similar get method,
+ * Finally, you can retrieve any asset with a similarly named get method:
  *
  * ```php
  * Keystone\Asset::getCss();
