ec351653009c2c53dbabeff7016e4b3fd4616d29
Mark Huot <mark@markhuot.com>
2013-02-21 07:24:33 -0500

    adding second layout

--- plugins/home/start.php
+++ plugins/home/start.php
@@ -3,8 +3,8 @@
 use Keystone\LayoutManager;
 use Keystone\Screen;
 
-// LayoutManager::register('markhuot.home')
-//   ->setLabel('Home')
-//   ->setPath(__DIR__)
-//   ->addScreen(Screen::makeWithName('content', 'content.twig', 'Content'))
-// ;
+LayoutManager::register('markhuot.home')
+  ->setLabel('Home')
+  ->setPath(__DIR__)
+  ->addScreen(Screen::makeWithName('content', 'content.twig', 'Content'))
+;
